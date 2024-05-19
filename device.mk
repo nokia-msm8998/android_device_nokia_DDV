5ee68c6f840c7253106764c2636905faeccdc4cf
Tuan Anh <tuan73176@gmail.com>
2024-05-19 14:05:48 +0700

    DDV: Set up initial device makefile configurations
    Change-Id: Id8161433844870efc44acc8351fa0fbba78a4463

---
+++ device.mk
@@ -0,0 +1,23 @@
+#
+# Copyright (C) 2020 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+DEVICE_PATH := device/nokia/DDV
+
+# Inherit from the common device configuration.
+TARGET_NOKIA_PLATFORM := sdm660
+$(call inherit-product, device/nokia/msm8998-common/msm8998-common.mk)
+
+# Overlay
+DEVICE_PACKAGE_OVERLAYS += \
+    $(DEVICE_PATH)/overlay \
+    $(DEVICE_PATH)/overlay-lineage
+
+# Soong
+PRODUCT_SOONG_NAMESPACES += \
+    $(DEVICE_PATH)
+
+# Inherit device specific vendor makefiles
+$(call inherit-product, vendor/nokia/DDV/DDV-vendor.mk)
