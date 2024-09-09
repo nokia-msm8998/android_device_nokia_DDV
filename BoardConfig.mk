74c54f2e58d04fc328525117cb243e1b452c54d6
Tuan Anh <tuan73176@gmail.com>
2024-09-09 09:22:24 +0700

    DDV: Build dtbo and vbmeta partitions
    DDV/SLD are two targets that features a dtbo and vbmeta
    partitions in nokia-msm8998 umbrella.
    
    Change-Id: I356f4d92a3111b1557268bc754c3907c0400fde7

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -7,6 +7,9 @@
 # Assert
 TARGET_OTA_ASSERT_DEVICE := DDV,DDV_sprout,Daredevil
 
+# A/B
+AB_OTA_PARTITIONS += dtbo vbmeta
+
 # QCOM
 TARGET_BOARD_PLATFORM := sdm660
 
@@ -20,6 +23,7 @@ include device/nokia/msm8998-common/BoardConfigCommon.mk
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
 
 # Kernel
+BOARD_KERNEL_SEPARATED_DTBO := true
 TARGET_KERNEL_CONFIG := lineageos_DDV_defconfig
 
 # Props
@@ -27,9 +31,13 @@ TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
 # Partitions
+BOARD_DTBOIMG_PARTITION_SIZE := 8388608
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 2684354560
 BOARD_VENDORIMAGE_PARTITION_SIZE := 1073741824
 BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
+
+# Recovery
+BOARD_INCLUDE_RECOVERY_DTBO := true
 
 # Inherit the proprietary files
 include vendor/nokia/DDV/BoardConfigVendor.mk
